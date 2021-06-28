34ed8cadd84217c00d705255cf13f4b9963916f3
paopapaopao <79187858+paopapaopao@users.noreply.github.com>
2021-06-28 20:10:53 +0800

    Updated: method name

--- rubyactivities/Confection.rb
+++ rubyactivities/Confection.rb
@@ -1,11 +1,11 @@
 class Confection
-    def bake()
+    def prepare()
         return ("Baking at 350 degrees for 25 minutes.")
     end
 end
 
 class Cupcake < Confection
-    def bake()
+    def prepare()
         return (super + " Applying frosting.")
     end
 end
@@ -16,5 +16,5 @@ end
 cup_cake = Cupcake.new()
 banana_cake = BananaCake.new()
 
-puts("Cupcake:\t" + cup_cake.bake())
-puts("Banana cake:\t" + banana_cake.bake())
+puts("Cupcake:\t" + cup_cake.prepare())
+puts("Banana cake:\t" + banana_cake.prepare())
